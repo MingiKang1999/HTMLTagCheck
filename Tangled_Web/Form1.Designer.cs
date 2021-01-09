86a41e0793383eb2357fab409d1c9a0ede25b0b3
Mingi Kang <77114865+MingiKang1999@users.noreply.github.com>
2021-01-09 17:06:04 -0500

    Add files via upload

---
+++ Tangled_Web/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Tangled_Web
+{
+    partial class fileCheckerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileLoadMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileExitMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.processMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.processCheckMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fileListBox = new System.Windows.Forms.ListBox();
+            this.fileMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileMenuStrip
+            // 
+            this.fileMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.fileMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenu,
+            this.processMenu});
+            this.fileMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.fileMenuStrip.Name = "fileMenuStrip";
+            this.fileMenuStrip.Padding = new System.Windows.Forms.Padding(11, 3, 0, 3);
+            this.fileMenuStrip.Size = new System.Drawing.Size(387, 30);
+            this.fileMenuStrip.TabIndex = 0;
+            this.fileMenuStrip.Text = "menuStrip1";
+            // 
+            // fileMenu
+            // 
+            this.fileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileLoadMenu,
+            this.fileExitMenu});
+            this.fileMenu.Name = "fileMenu";
+            this.fileMenu.Size = new System.Drawing.Size(46, 24);
+            this.fileMenu.Text = "&File";
+            // 
+            // fileLoadMenu
+            // 
+            this.fileLoadMenu.Name = "fileLoadMenu";
+            this.fileLoadMenu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.fileLoadMenu.Size = new System.Drawing.Size(210, 26);
+            this.fileLoadMenu.Text = "&Load File...";
+            this.fileLoadMenu.Click += new System.EventHandler(this.fileLoadMenu_Click);
+            // 
+            // fileExitMenu
+            // 
+            this.fileExitMenu.Name = "fileExitMenu";
+            this.fileExitMenu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.fileExitMenu.Size = new System.Drawing.Size(210, 26);
+            this.fileExitMenu.Text = "E&xit";
+            this.fileExitMenu.Click += new System.EventHandler(this.fileExitMenu_Click);
+            // 
+            // processMenu
+            // 
+            this.processMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.processCheckMenu});
+            this.processMenu.Name = "processMenu";
+            this.processMenu.Size = new System.Drawing.Size(72, 24);
+            this.processMenu.Text = "&Process";
+            // 
+            // processCheckMenu
+            // 
+            this.processCheckMenu.Enabled = false;
+            this.processCheckMenu.Name = "processCheckMenu";
+            this.processCheckMenu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.processCheckMenu.Size = new System.Drawing.Size(215, 26);
+            this.processCheckMenu.Text = "&Check Tags";
+            this.processCheckMenu.Click += new System.EventHandler(this.processCheckMenu_Click);
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.fileLabel.Location = new System.Drawing.Point(13, 39);
+            this.fileLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(150, 20);
+            this.fileLabel.TabIndex = 1;
+            this.fileLabel.Text = "No File Loaded";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "HTML files(*.htm; *html)|*.html";
+            this.openFileDialog.InitialDirectory = "TestFiles";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // fileListBox
+            // 
+            this.fileListBox.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileListBox.FormattingEnabled = true;
+            this.fileListBox.ItemHeight = 21;
+            this.fileListBox.Location = new System.Drawing.Point(17, 94);
+            this.fileListBox.Name = "fileListBox";
+            this.fileListBox.Size = new System.Drawing.Size(314, 256);
+            this.fileListBox.TabIndex = 2;
+            // 
+            // fileCheckerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 394);
+            this.Controls.Add(this.fileListBox);
+            this.Controls.Add(this.fileLabel);
+            this.Controls.Add(this.fileMenuStrip);
+            this.Font = new System.Drawing.Font("Haan Baekje B", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.MainMenuStrip = this.fileMenuStrip;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "fileCheckerForm";
+            this.Text = "Harry\'s Simple HTML File Checker";
+            this.fileMenuStrip.ResumeLayout(false);
+            this.fileMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip fileMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileMenu;
+        private System.Windows.Forms.ToolStripMenuItem processMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileLoadMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileExitMenu;
+        private System.Windows.Forms.ToolStripMenuItem processCheckMenu;
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ListBox fileListBox;
+    }
+}
+
---
+++ Tangled_Web/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+/// <summary>
+/// Author: Mingi Kang
+/// 
+/// File Date: 11/22/2020
+/// 
+/// Purpose of the class: This form is designed to check if the html file is balanced properly.
+/// If corresponding html tags are opened and closed the file has passed the test, fail otherwise.
+/// </summary>
+namespace Tangled_Web
+{
+    /// <summary>
+    /// This form reads from the chosen html file using openFileDialog
+    /// </summary>
+    public partial class fileCheckerForm : Form
+    {
+        private StreamReader htmlFile; //html file chosen from the openFileDialog
+
+        /// <summary>
+        /// Main form that displays choice of selections for the user
+        /// </summary>
+        public fileCheckerForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Closing the form
+        /// </summary>
+        /// <param name="sender">User selecting the exit menu</param>
+        /// <param name="e">The exit menu has been selected</param>
+        private void fileExitMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Loading the html file behind the form, ready to be processed
+        /// </summary>
+        /// <param name="sender">User clicking file load menu</param>
+        /// <param name="e">the file load menu has been selected</param>
+        private void fileLoadMenu_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK) //The user has selected a valid file
+            {
+                var fileStream = openFileDialog.OpenFile(); //Store the file
+
+                htmlFile = new StreamReader(fileStream); //Store the file into object variable
+
+                fileLabel.Text = "Loaded: " + Path.GetFileName(openFileDialog.FileName); //Indicate that the file has sucessfully loaded
+                processCheckMenu.Enabled = true; //Allow the user to select the process menu
+            }
+            else //User did not select a file
+            {
+                MessageBox.Show("You did not select a file!");
+            }
+        }
+
+        /// <summary>
+        /// Not necessary, just for testing purposes
+        /// </summary>
+        /// <param name="sender">User Clicks to load a file</param>
+        /// <param name="e">Event ends once file is selected</param>
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Process and determine whether the fiile is balanced correctly
+        /// </summary>
+        /// <param name="sender">User clicking the process menu</param>
+        /// <param name="e">The process menu has been selected</param>
+        private void processCheckMenu_Click(object sender, EventArgs e)
+        {
+            var fileContent = htmlFile.ReadToEnd(); //Store the content of the file into a local variable 
+
+            CheckTags(fileContent); //Pass the string of html file content into the CheckTags method
+        }
+
+        /// <summary>
+        /// Once tag has been opened look for end tag, and if the end tag
+        /// does not exit the file is not balanced
+        /// </summary>
+        /// <param name="fileContent">String content of the html file</param>
+        private void CheckTags(string fileContent)
+        {
+            fileListBox.Items.Clear(); //Clear the list box items each time process menu has been selected
+            string tag = ""; //Create a local tag variable
+            int i = 0; //Used to check the index of the characters
+            bool openClose = true; //Used to check if the tag is open or closed
+            bool withVal = true; //Used to check if there is a value with the open tag
+            Stack<string> tags = new Stack<string>(); //Stack list to compare the open tag with the closed tag
+
+            fileContent = fileContent.ToLower(); //Change all the characters to lower case
+
+            char[] fileCharacters = fileContent.ToCharArray(); //Create an array of characters of the string html file content
+
+            while (i < fileCharacters.Length) //Runs until the last character
+            {
+                openClose = true;
+                withVal = true;
+                if (fileCharacters[i] == '<') //Tag has been found
+                {
+                    while (fileCharacters[i] != '>') //Loop until the end of the tag
+                    {
+                        if (fileCharacters[i] == ' ') //If there is a space to indicate a value to the tag
+                        {
+                            tag += ">";
+                            fileListBox.Items.Add("Found opening tag: " + tag); //Format the current tag and display it on the list box
+                            tags.Push(tag); //Add the tag to the top of the tags Stack
+                            while (fileCharacters[i] != '>') //Force to close to remove the value from the tag
+                            {
+                                    i++;
+                            }
+                            tag = "";
+                            i = i - 1;
+                            withVal = false; //Indicate that this tag had values
+                        }
+                        if ((fileCharacters[i + 1] == 'b' && fileCharacters[i + 2] == 'r') ||
+                            (fileCharacters[i + 1] == 'h' && fileCharacters[i + 2] == 'r') || (fileCharacters[i + 1] == 'i' && fileCharacters[i + 2] == 'm')) //If tag is not a container
+                        {
+                            tags.Pop(); //Automatically remove the tag from the Stack
+                            while (fileCharacters[i] != '>') //Run until the end of the tag
+                            {
+                                tag += Convert.ToString(fileCharacters[i]);
+                                i++;
+                                if (fileCharacters[i] == ' ') //If non-container tag has a value
+                                {
+                                    tag += ">";
+                                    fileListBox.Items.Add("Found non-container tag: " + tag); //Format the current tag and display it on the list box
+                                    while (fileCharacters[i] != '>') //Force to close to remove the value from the tag
+                                    {
+                                        i++;
+                                    }
+                                    tag = "";
+                                    i = i - 1;
+                                    withVal = false; //Indicate that this tag had values
+                                }
+                            }
+                            if (fileCharacters[i] == '>') //Tag is a non-container with no values
+                            {
+                                tag += ">";
+                            }
+
+                            if (withVal) //Add the non-container tag with no value to the Stack and display it on list box
+                            {
+                                fileListBox.Items.Add("Found non-container tag: " + tag);
+                                tags.Push(tag);
+                            }
+                            openClose = false; //If non of the above applies it is a closed tag
+                            tag = "";
+                            i = i - 1;
+
+                        }
+
+                        if (fileCharacters[i + 1] == '/') //Indication for closed tags
+                        {
+                            i = i + 2; //This is to only store the elements need to compare to the Stack
+                            while (fileCharacters[i] != '>') //Collect the content of the tag without /
+                            {
+                                tag += Convert.ToString(fileCharacters[i]);
+                                i++;
+                            }
+                            if (fileCharacters[i] == '>') //Add the closed tag manually
+                            {
+                                tag += ">";
+                            }
+
+                            fileListBox.Items.Add("Found closing tag: " + "</" + tag); //Display the closed tag on the list box
+                            tag = "<" + tag;
+                            if (tag == tags.Peek()) //Check to see if the closed tag a corresponding open tag
+                            {
+                                tags.Pop(); //Remove the balanced tag
+                            }else if(tag != tags.Peek() && tags.Count != 1) //If there is a balance error
+                            {
+                                goto LoopEnd; //Break out of the nested loop
+                            }
+                            openClose = false; //Tag is still closed
+                            tag = "";
+                            i = i - 1;
+                        }
+                        tag += Convert.ToString(fileCharacters[i]); //This is collecting for the open tag
+                        i++;
+                    }
+                    if (fileCharacters[i] == '>') //Finish collecting open tag
+                    {
+                        tag += ">";
+                    }
+                }
+                if (openClose && tag != "" && withVal) //Check if the tag is open with no values
+                {
+                    fileListBox.Items.Add("Found opening tag: " + tag);
+                    tags.Push(tag); //Stack the open tag to the top
+                }
+                tag = "";
+                i++;
+            }
+            LoopEnd: //Break out point
+            if (tags.Count == 0) //Indication that all tags were properly closed
+            {
+                fileLabel.Text = Path.GetFileName(openFileDialog.FileName) + " has balanced tags"; //Display a label to tell the user that html file is balanced
+            }
+            else //If there was an unclosed tag
+            {
+                fileLabel.Text = Path.GetFileName(openFileDialog.FileName) + " does not have balanced tags"; //Display a label to tell the user that html file is not balanced
+            }
+        }
+    }
+}
